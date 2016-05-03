7ae86ece863aba9b2d4af6a3109bb359ce34fd06
Emmaer <854477933@qq.com>
2016-05-03 11:03:41 +0800

    e

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    direction: 'vertical',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 30,
+    mousewheelControl: true
+});/**
+ * Created by hp on 2016/5/2.
+ */
+}
